93a3633ae557585a50708c4107056187292f5ff2
“FreshmanGGboy <346606470@qq.com>
2026-01-06 19:44:08 +0800

    基础文章发布

---
+++ src/apis/article.jsx
@@ -0,0 +1,22 @@
+// 用户 相关的所有请求
+import { request } from "../utils"
+
+// 1.获取频道列表
+export function getChannelApi(){
+  return request({
+    url:'/channels',
+    method:'GET',
+  })
+} 
+
+
+// 2.提交文章表单
+
+
+export function createAreticleApi(data){
+  return request({
+    url: '/mp/articles?draft=false',
+    method: 'POST',
+    data
+  })
+} 
